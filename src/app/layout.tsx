0ba7dcd08995a3462ce8350ba74e5b271f65c572
ExtremeStylez <extremestylez786@gmail.com>
2026-04-06 01:19:21 +0200

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", jetBrainsMono.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
---
+++ src/components/auth-card.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function AuthCard() {
+    return (<Card className="w-full max-w-md border-border/60 bg-card shadow-2xl shadow-primary/5">
+        
+    </Card>)
+}
